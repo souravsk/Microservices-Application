4f5d0233b8ffca6a63aed97fdd46d9ce8254ac6c
souravsk <souravk326@gmail.com>
2022-11-29 15:10:25 +0530

    compeleted inventory

--- Inventory/consumer.py
+++ Inventory/consumer.py
@@ -7,13 +7,22 @@ group = 'inventory-group'
 try:
     redis.xgroup_create(key, group)
 except:
-    print('Group already exists!')\
+    print('Group already exists!')
 
 while True:
     try:
         results = redis.xreadgroup(group,key, {key:'>'}, None)
 
-        print(results)
+        if results != []:
+            for result in results:
+                obj = result[1][0][1]
+                try:
+                    product = Product.get(obj['product_id'])
+                    product.quantity = product.quantity - int(obj['quantity'])
+                    product.save()
+                except:
+                    redis.xadd('refund_order', obj, '*')
+
     except Exception as e:
         print(str(e))
     time.sleep(1)
