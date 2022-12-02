edecaf705493f8bb6574fcca189a4882aa910220
souravsk <souravk326@gmail.com>
2022-12-02 20:21:33 +0530

    created files

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,55 +1,12 @@
+import { Products } from "./components/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 'react-router-dom'
+
 function App() {
-  return(<div className="App">
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
-        <div class="navbar-nav">
-          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Sign out</a>
-          </div>
-        </div>
-      </header>
-      <div class="container-fluid">
-        <div class="row">
-          <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div class="position-sticky pt-3 sidebar-sticky">
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    <span data-feather="home" class="align-text-bottom"></span>
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div class="table-responsive">
-              <table class="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
-    </div>
-  );
+  return <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products}
+      </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
